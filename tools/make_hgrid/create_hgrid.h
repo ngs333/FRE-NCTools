34a8f47e17c042f6a2c20dc3cd297610b9b3fe63
Miguel Zuniga <Miguel.Zuniga@noaa.gov>
2021-05-17 23:49:43 -0400

    Version with original Zhi make_hgrid in 2021 build system.

--- tools/make_hgrid/create_hgrid.h
+++ tools/make_hgrid/create_hgrid.h
@@ -19,7 +19,7 @@
  **********************************************************************/
 /*******************************************************************************
                              create_hgrid.h
-  This header file provide interface to create different types of horizontal 
+  This header file provide interface to create different types of horizontal
   grid. geographical grid location, cell length, cell area and rotation
   angle are returned. All the returned data are on supergrid.
 
@@ -61,14 +61,13 @@ void create_tripolar_grid( int *nxbnds, int *nybnds, double *xbnds, double *ybnd
 void create_conformal_cubic_grid( int *npts, int *nratio, char *method, char *orientation, double *x,
 			          double *y, double *dx, double *dy, double *area, double *angle_dx,
 			          double *angle_dy );
-
 void create_gnomonic_cubic_grid( char* grid_type, int *nlon, int *nlat, double *x, double *y,
-				 double *dx, double *dy, double *area, double *angle_dx,
-			         double *angle_dy, double shift_fac, int do_schmidt, int do_cube_transform, double stretch_factor,
-				 double target_lon, double target_lat, int nest_grids,
-				 int parent_tile[MAX_NESTS], int refine_ratio[MAX_NESTS], int istart_nest[MAX_NESTS],
-				 int iend_nest[MAX_NESTS], int jstart_nest[MAX_NESTS], int jend_nest[MAX_NESTS],
-				 int halo, int output_angle_length );
+				double *dx, double *dy, double *area, double *angle_dx,
+				 double *angle_dy, double shift_fac, int do_schmidt, int do_cube_transform, double stretch_factor,
+				 double target_lon, double target_lat, int nest_grid,
+				 int parent_tile, int refine_ratio, int istart_nest,
+                                 int iend_nest, int jstart_nest, int jend_nest, int halo, int output_length_angle);
+
 void create_f_plane_grid( int *nxbnds, int *nybnds, double *xbnds, double *ybnds,
                           int *nlon, int *nlat, double *dlon, double *dlat,
 			  int use_legacy, double f_plane_latitude, int *isc, int *iec,
